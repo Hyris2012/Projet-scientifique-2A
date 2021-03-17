ad9243a96e75cbfaafc019e0662c4697072aa1f1
xyzser <eliettelebrun@MacBook-Air-de-Eliette.local>
2021-03-17 19:03:20 +0100

    parcours et dessin de la trajectoire
    j'ai supposé que
    1/ balle implémente des get pour les arraylist qu'elle a en attributs private
    2/ elle a deux attributs atterrissage et max qui donnent la longueur parcourue (= racine positive du polynôme) et l'altitude maximum atteinte
    3/ on ouvre la fenetreFinJeu quand la balle a atterri, et elle affiche des informations du style de la longueur parcourue, l'altitude max atteinte et donc les prend en paramètre
    
    c'était juste pour pouvoir écrire toute la structure du programme, on pourra revoir ça
    
    et en fait j'ai pas trouvé d'autre solution pour le moment que de créer ici aussi des attributs en arraylist et de les remplir au fur et à mesure que le timer avance... :/

--- PanelTraj.java
+++ PanelTraj.java
@@ -1,11 +1,75 @@
 /*
  *
+ * qq problèmes :
+ * histoire d'échelle pour que tout soit toujours contenu entier dans le panel
+ * 
+ * il faut avoir accès aux arraylist de la balle -> passer en public ou écrire un get 
+ * il faut connaitre la racine positive du polynôme, càd là où elle touche le sol (attribut nommé 'atterissage' ici en attendant)
+ * 
+ * 
+ * 
+ * 
  */
 
 
-public class PanelTraj {
+public class PanelTraj extends JPanel implements ActionListener {
 	
-	public PanelTraj() {
+	private Balle balle;
+	//private ImageIcon fond;
+	public Timer time;
+	public int tps;
+	public ArrayList<Double> Xparcourus;
+	public ArrayList<Double> Yparcourus;
+	
+	public PanelTraj(Balle balle) {
+		super();
+		setLayout(null);
+		setBounds(50, 50, 500, 700);
+		setBackground(Color.black);
+		
+		time = new Timer (100, this);
+	
+		balle = new Balle();
+		
+		Xparcourus = new <Double> ArrayList();
+		Yparcourus = new <Double> ArrayList();
+		
+	}
+	
+	public String toString(){
+		//juste pour le principe
+	}
+	
+	public void lancerBalle(Balle balle){
+		this.balle = balle;
+		time.start();
+	}
+	
+	public void paint(Graphics g){
+		
+		// appelé lors de l'appui sur le bouton 'lancer' -> càd dans actionPerformed de FenetreJeu 
+		
+		// dessine la trajectoire jusqu'à --> tps 
+		g.setColor(Color.white);
+		drawPolyline(balle.valeurX.toArray(), balle.valeurY.toArray());
+		
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+		if(!aterrie()){
+			Xparcourus.add(balle.getValeurX().get(tps)
+			paint();
+		}else{
+			new FenetreFinJeu(balle.aterrissage, balle.max);	// en supposant que la fenetre affiche l'altitude max atteinte et la longueur parcourue, qui seraient tout deux attributs de balle 
+		}
+		
+	}
+	
+	public boolean atterrie(){
+		// doit déterminer si la balle a atteri 
+		boolean b = ((Xparcourus.get(Xparcourus.size()-1) >= balle.atterrissage);
+		return b; 
 		
 	}
 }
