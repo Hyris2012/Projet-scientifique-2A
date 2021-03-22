0602d9bf5c86861d9f804f64c94e73d01780b1db
Hyris2012 <mariesara.mazen@yahoo.com>
2021-03-22 16:00:14 +0100

    Création Vecteur et APoint
    Création de APoint (même que dans le TP courbes) et vréation de vecteur pour initialiser les paramètres du tir

---
+++ Vecteur.java
@@ -0,0 +1,19 @@
+public class Vecteur {
+	private APoint base; // à voir si on fait une balle qui s'initialise toujours au même endroit ou pas (ça serait mieux peut etre)
+	private APoint pointe; // bout de la flèche 
+	private double module;
+	private double argument; // pour récupérer l'angle fait par la souris 
+	
+	public Vecteur(){
+	}
+	
+	// méthode pour récuper le module --> méthode distance dans la classe APoint
+	
+	
+	// méthode pour récuperer l'argument 
+	
+
+	
+	
+}
+
