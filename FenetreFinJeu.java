a3b21a056c56993cb619cf89cb56a26ed23ab2a1
xyzser <eliette.lebrun@insa-lyon.fr>
2021-03-24 21:30:47 +0100

    création polynôme
    Création d'une classe polynome pour alléger l'écriture de la classe balle : méthodes et attributs utiles =
    - constructeur : les 3 coeffs a, b, c en paramètre, dans cet ordre
    - pour calculer les valeur f(x) : calculFdeX : double
    - accès au lieu d'atterrissage (= racine positive) : calculAtterrissage()
    - accès à l'abcisse du sommet : getAlpha() : double
    - accès à l'ordonnée du sommet : calculSommet() : double
    
    constructeur vide disponible

--- FenetreFinJeu.java
+++ FenetreFinJeu.java
@@ -2,7 +2,11 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class FenetreFinJeu {
+public class FenetreFinJeu extends JFrame {
+	
+	public FenetreFinJeu(String type, String affichage){
+		super(type);
+	}
 	
 	
 }
