92178913f857f1aefcd5979b536a250069f5e9c8
Luc4s29 <lucas.tallin@insa-lyon.fr>
2021-03-24 14:38:59 +0100

    ihm de base (il manque la fenêtre de jeu et affichage équation)

--- FenetreJeu.java
+++ FenetreJeu.java
@@ -4,54 +4,96 @@ import java.awt.*;
 
 public class FenetreJeu extends JFrame{
 	private JPanel FenPrinc ; 
-	private JPanel paramG ;
+	//private JPanel paramG ;
 	private JPanel courbe ; 
-	private JPanel equa ;
+	private JLabel equa ;
 	private JButton lancer ;
+	private int largeur = 1500;
+	private int hauteur = 1000; 
+	public int score1 ;
+	public int vie1 ; 
 
 	public FenetreJeu () {
 		super("Trajectory Manager") ;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(180,10,largeur,hauteur);
 		
 		FenPrinc = new JPanel() ; 
 		FenPrinc.setLayout(null) ; 
+		FenPrinc.setBounds(0,0,largeur,hauteur) ;
+		FenPrinc.setBackground(Color.white) ; 
 		
-		paramG = new JPanel() ; 
+		/* paramG = new JPanel() ; 
 		paramG.setLayout(null) ;
-		paramG.setBounds(15,15,800,250) ;
-		paramG.setBackground(Color.blue) ; 
+		paramG.setBounds((int)(largeur*(0.2/29.7)),(int)(hauteur*(0.5/21.0)),(int)(largeur*(8.0/29.7)),(int)(hauteur*(19.0/21.0))) ;
+		paramG.setBackground(Color.green) ; */
 		
-		JLabel angle = new JLabel() ;
-		angle.setBounds(10,10,230,50);
-		angle.setText("Saisir l'angle choisi en degrés :") ;
+		JLabel angle = new JLabel("Saisir l'angle choisi en degres :") ; //1
+		angle.setFont(new Font("Arial",Font.BOLD,20)) ;
+		angle.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(0.7/21)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0)));
 		
-		JSlider angle1 = new JSlider(0,90) ;
-		angle1.setBounds(10,70,230,20) ;
+		JSlider angle1 = new JSlider(0,90,45) ; //2
+		angle1.setMajorTickSpacing(15);
+		angle1.setPaintLabels(true) ; 
+		angle1.setPaintTicks(true) ; 
+		angle1.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(2.5/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ;
 		
-		JLabel vitesse = new JLabel() ; 
-		vitesse.setBounds(10,100,230,50) ; 
-		vitesse.setText("saisir la vitesse initiale choisie (m/s) :") ;
+		JLabel vitesse = new JLabel("saisir la vitesse initiale choisie (m/s) :") ; //3
+		vitesse.setFont(new Font("Arial",Font.BOLD,20)) ;
+		vitesse.setBounds((int)(largeur*(0.7/21.0)),(int)(hauteur*(4.3/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ; 
 		
-		JLabel score = new JLabel() ; 
-		score.setBounds(10,160,230,50) ;
-		score.setText(" Score : X") ; 
+		JSlider vitesse1 = new JSlider(0,90,45) ; //4
+		vitesse1.setMajorTickSpacing(15);
+		vitesse1.setPaintLabels(true) ;
+		vitesse1.setPaintTicks(true) ; 
+		vitesse1.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(6.1/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ;
 		
-		JLabel vie = new JLabel() ; 
-		vie.setBounds(10,220,230,50) ;
-		vie.setText("Encore Y vies") ; 
+		score1 = 0 ; 
+		JLabel score = new JLabel(" Score : "+score1) ; //5
+		score.setFont(new Font("Serif",Font.BOLD,36)) ;
+		score.setBounds((int)(largeur*(3/29.7)),(int)(hauteur*(7.9/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ;
 		
-		lancer = new JButton() ; 
-		lancer.setBounds(10,290,230,50) ;
-		lancer.setText("Lancer !") ; 
+		vie1 = 0 ;
+		JLabel vie = new JLabel("Encore "+vie1+" vies") ; //6
+		vie.setFont(new Font("Serif",Font.BOLD,36)) ;
+		vie.setBounds((int)(largeur*(3/29.7)),(int)(hauteur*(9.7/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ;
+		
+		JLabel objet = new JLabel("Choisissez l'objet :") ; //7
+		objet.setFont(new Font("Arial",Font.BOLD,20)) ;
+		objet.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(11.5/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ;
+		
+		JComboBox objet1 = new JComboBox() ; //8
+		objet1.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(13.3/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1/21.0))) ;
+		
+		JLabel endroit = new JLabel("Choisissez l'endroit :") ; //9
+		endroit.setFont(new Font("Arial",Font.BOLD,20)) ;
+		endroit.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(14.6/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ;
+		
+		JComboBox endroit1 = new JComboBox() ; //10
+		endroit1.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(16.4/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1/21.0))) ;
+		
+		lancer = new JButton("Jouer ! ") ; //11
+		lancer.setFont(new Font("Stencil",Font.BOLD,50)) ;
+		lancer.setBounds((int)(largeur*(0.7/29.7)),(int)(hauteur*(17.7/21.0)),(int)(largeur*(8/29.7)),(int)(hauteur*(1.5/21.0))) ;
 		lancer.setBackground(Color.red) ; 
 		
-		paramG.add(angle) ;
-		paramG.add(angle1) ;
-		paramG.add(vitesse) ; 
-		paramG.add(score) ;
-		paramG.add(vie) ; 
-		paramG.add(lancer) ; 
-		FenPrinc.add(paramG) ;
+		equa = new JLabel("Equation de la trajectoire : ") ; //12
+		equa.setBounds((int)(largeur*(15/29.7)),(int)(hauteur*(15/21.0)),(int)(largeur*(15/29.7)),(int)(hauteur*(3/21.0))) ; 
+		equa.setFont(new Font("Arial",Font.BOLD,32)) ;
+		
+		FenPrinc.add(angle) ; //1
+		FenPrinc.add(angle1) ; //2
+		FenPrinc.add(vitesse) ; //3
+		FenPrinc.add(vitesse1) ; //4
+		FenPrinc.add(score) ; //5
+		FenPrinc.add(vie) ; //6
+		FenPrinc.add(objet) ; //7
+		FenPrinc.add(objet1) ; //8 
+		FenPrinc.add(endroit) ; //9
+		FenPrinc.add(endroit1) ; //10
+		FenPrinc.add(lancer) ; //11
+		FenPrinc.add(equa) ; //12
+		this.add(FenPrinc) ;
 		setVisible(true) ;  
 	}
 }
