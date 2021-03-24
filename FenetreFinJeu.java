5d94f398e2be27e3fc013fec15e5c6a8f5dc01dc
xyzser <eliette.lebrun@insa-lyon.fr>
2021-03-24 21:48:10 +0100

    fenetreFinJeu

--- FenetreFinJeu.java
+++ FenetreFinJeu.java
@@ -6,8 +6,33 @@ public class FenetreFinJeu extends JFrame {
 	
 	public FenetreFinJeu(String type, String affichage){
 		super(type);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(550, 400, 400, 200);
+		
+		JLabel etiquette = new JLabel("affichage");
+		
+		JPanel unique = new JPanel();
+		unique.setLayout(null);
+		unique.setBackground(Color.blue);
+		
+		JPanel second = new JPanel();
+		second.setLayout(null);
+		second.setBounds(60, 40, 280, 100);
+		second.setBackground(Color.white);
+		
+		second.add(etiquette);
+		
+		unique.add(second);
+		
+		add(unique);
+		
+		setVisible(true);
+		
 	}
 	
+	public static void main (String[] args){
+		new FenetreFinJeu("Test", "Hello world");
+	}
 	
 }
 
